819026fe114cade543f4f90a9fb0e4c257c7c8f7
fresco <fresco@frescos-iMac.local>
2019-10-09 11:03:57 -0400

    finished functionality in components, mvp complete

--- reducer-todo/src/components/TodoCard.jsx
+++ reducer-todo/src/components/TodoCard.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 
 
 export const TodoCard = ( { task, dispatch }) => {
+
+    const markCompleted = () => {
+        const element = document.getElementById(`${task.task}`)
+        element.classList.toggle('completed')
+        dispatch ( { type: 'MARK_COMPLETED', payload: task.id } )
+    }
+
     return (
         <div className='todo-card'>
-            <div className="todo-text">
-                <h3>Task...</h3>
-            </div>
-            <button>remove</button>
+            <h3 onClick={markCompleted} id={task.task}>{task.task}</h3>
         </div>
     )
 }
--- reducer-todo/src/components/TodoList.jsx
+++ reducer-todo/src/components/TodoList.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import { TodoCard } from './TodoCard'
 
 export const TodoList = ( { list, dispatch } ) => {
-    
+    const removeCompleted = () => {
+        dispatch( { type: 'REMOVE_COMPLETED' } )
+    }
 
     return (
         <div>
             {console.log('LIST: ', list)}
             {list.tasks.map( task => <TodoCard key={task.id} task={task} dispatch={dispatch}/>) }
-            <button onClick=''></button>
+            <button onClick={removeCompleted}></button>
         </div>
     )
 }
