c146be0ed7e7f57ae831a34f00934417340c13d0
fresco <fresco@frescos-iMac.local>
2019-10-09 09:50:30 -0400

    reducer created

--- reducer-todo/src/reducers/TodoReducer.js
+++ reducer-todo/src/reducers/TodoReducer.js
@@ -12,5 +12,30 @@ export const initialState = {
 }
 
 export const reducer = ( state, action ) => {
+    // switch
+    switch(action.type) {
+        case "ADD_TASK":
+            return {
+                tasks: [
+                    ...state.tasks, { id: Date.now(), task: action.payload, completed: false }
+                ]
+            }
+        case "MARK_COMPLETED":
+            return {
+                tasks: [
+                    state.tasks.map( task => 
+                        task.id === action.payload
+                        ? { ... task, completed: !task.completed } 
+                        : task
+                        )
+                ]
+            }
+        case "REMOVE_COMPLETED":
+            return {
+                tasks: [
+                    state.tasks.filter( task => !task.completed )
+                ]
+            }
+    }
 
 }
